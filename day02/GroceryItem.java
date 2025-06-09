ea8d0700603ffcf2bd4783713854fb68597d8818
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2025-06-09 14:09:32 +0530

    correct class

--- day02/GroceryItem.java
+++ day02/GroceryItem.java
@@ -8,8 +8,8 @@ public class GroceryItem {
         int[] quantity = new int[7];
         int total = 0;
         Scanner scanner = new Scanner(System.in);
-        int max = max(quantity);
-        int min = min(quantity);
+        int max = quantity[0];
+        int min = quantity[0];
 
         for (int i = 0; i < Days.length; i++) {
             System.out.print("Enter the quantity of items sold on " + Days[i] + ": ");
@@ -17,9 +17,19 @@ public class GroceryItem {
         }
         for (int i = 0; i < quantity.length; i++) {
             total += quantity[i];
-            System.out.println("Total quantity of items sold: " + total);
-            System.out.println("Average quantity of items sold: " + (total / 7));
+
         }
+
+        for (int i = 1; i < quantity.length; i++) {
+            if (quantity[i] < min) {
+                min = quantity[i];
+            }
+            if (quantity[i] > max) {
+                max = quantity[i];
+            }
+        }
+        System.out.println("Total quantity of items sold: " + total);
+        System.out.println("Average quantity of items sold: " + (total / 7));
 
         System.out.println("Maximum quantity of items sold in a day: " + max);
         System.out.println("Minimum quantity of items sold in a day: " + min);
