039db753e49379f556038316ebf0039405c58e21
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2025-06-11 18:34:52 +0530

    Add queastion 10

---
+++ day05/ProcessNumbers.java
@@ -0,0 +1,24 @@
+//queastion 10
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ProcessNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[7];
+        System.out.println("Enter the number of elements in the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("Original array: " + Arrays.toString(numbers));
+
+        Arrays.stream(numbers).forEach(System.out::println);
+        System.out.println("Squares of the numbers:");
+        Arrays.stream(numbers)
+                .map(n -> n * n)
+                .forEach(System.out::println);
+    }
+}
